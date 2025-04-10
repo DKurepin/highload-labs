5bfd5c642fadb45fd08be9a37109f171871d28d3
Vladislav Hober <vladislavhober@gmail.com>
2025-04-10 14:32:46 +0300

    use jetty

--- src/main/kotlin/ru/quipy/jetty.kt
+++ src/main/kotlin/ru/quipy/jetty.kt
@@ -1,11 +1,9 @@
-import org.apache.coyote.http2.Http2Protocol
 import org.eclipse.jetty.http2.server.HTTP2CServerConnectionFactory
 import org.springframework.boot.web.embedded.jetty.JettyServerCustomizer
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory
-import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer
 import org.springframework.context.annotation.Bean
 
-@Bean // это для Jetty
+@Bean
 fun jettyServerCustomizer(): JettyServletWebServerFactory {
     val jettyServletWebServerFactory = JettyServletWebServerFactory()
 
@@ -15,15 +13,4 @@ fun jettyServerCustomizer(): JettyServletWebServerFactory {
 
     jettyServletWebServerFactory.serverCustomizers.add(c)
     return jettyServletWebServerFactory
-}
-
-@Bean
-fun tomcatConnectorCustomizer(): TomcatConnectorCustomizer {
-    return TomcatConnectorCustomizer {
-        try {
-            (it.protocolHandler.findUpgradeProtocols().get(0) as Http2Protocol).maxConcurrentStreams = 10_000_000
-        } catch (e: Exception) {
-
-        }
-    }
 }
